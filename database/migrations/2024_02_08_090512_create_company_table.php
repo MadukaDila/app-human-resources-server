eb857e124e26187d7af8c400d324b0f65c38b886
Amila T Kumarasekara <42783431+MadukaDila@users.noreply.github.com>
2024-02-08 14:39:14 +0530

    create company table

---
+++ database/migrations/2024_02_08_090512_create_company_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id('company_id');
+            $table->string('company_name');
+            $table->string('company_email')->unique();
+            $table->string('company_phone');
+            $table->string('company_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company');
+    }
+};
